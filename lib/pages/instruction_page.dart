fb4d1b9e4271b2f1ff52fe0bc2499fc7dc7ab162
to-rex <torex.amaki@gmail.com>
2024-04-26 02:05:53 +0500

    first commit

--- lib/pages/instruction_page.dart
+++ lib/pages/instruction_page.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../controllers/get_controller.dart';
 
 class InstructionPage extends StatelessWidget {
+
+  final GetController _getController = Get.put(GetController());
+  late final WebViewController _controller;
+
+
   @override
   Widget build(BuildContext context) {
+    //google.com
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://help.netflix.com/legal/fastprivacy')) {
+              return NavigationDecision.prevent;
+            }else{
+              return NavigationDecision.navigate;
+            }
+          }
+        ),
+      )
+      ..loadRequest(Uri.parse('https://help.netflix.com/legal/fastprivacy'));
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Loyihalar'.tr),
+        title: Text('Foydalanish yo\'riqnomasi'.tr),
         centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.arrow_back, size: 30), onPressed: () => Get.back()),
+        leading: IconButton(icon: Icon(Icons.arrow_back, size: Get.width * 0.06), onPressed: () => Get.back())
       ),
       body: SafeArea(
         child: Center(
-          child: Text('Instruction Page'),
+          child: WebViewWidget(controller: _controller)
         )
       )
     );
