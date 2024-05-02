7ff9432554b7eed57a26e8bed251848ebd246fac
to-rex <torex.amaki@gmail.com>
2024-05-02 18:21:12 +0500

    first commit

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../controllers/get_controller.dart';
+
 class LoginPage extends StatelessWidget {
+
+  LoginPage({super.key});
+
+  final GetController _getController = Get.put(GetController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'.tr),
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back, size: Get.width * 0.07),
+            onPressed: () => Get.back())
       ),
-      body: Center(child: Text('Login'.tr)),
+      body: Column(
+        children: [
+          SizedBox(height: Get.height * 0.03),
+          Container(
+            width: Get.width * 0.8,
+            margin: EdgeInsets.symmetric(horizontal: Get.width * 0.03),
+            child: Text('Telefon raqamingizni kiriting:'.tr, style: TextStyle(fontSize: Get.width * 0.06, fontWeight: FontWeight.w500))
+          ),
+          Container(
+              width: Get.width * 0.8,
+              margin: EdgeInsets.only(top: Get.height * 0.01, left: Get.width * 0.03, right: Get.width * 0.03,bottom: Get.height * 0.01),
+              child: Text('Biz Tasdiqlash kodini jo‘natamiz.'.tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7), fontSize: Get.width * 0.045, fontWeight: FontWeight.w500))
+          ),
+
+        ]
+      )
     );
   }
 }
