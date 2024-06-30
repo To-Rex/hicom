f1c2a7aaec82da834a6299e9157f6ff1344f8427
to-rex <torex.amaki@gmail.com>
2024-07-01 02:51:50 +0500

    first commit

--- lib/companents/sample/port_items.dart
+++ lib/companents/sample/port_items.dart
@@ -7,8 +7,9 @@ import '../../controllers/get_controller.dart';
 class PortItems extends StatelessWidget {
 
   final List<String> data;
+  final List<int>? ports;
 
-  PortItems({super.key, required this.data});
+  PortItems({super.key, required this.data, this.ports});
 
   final GetController _getController = Get.put(GetController());
   @override
@@ -16,8 +17,8 @@ class PortItems extends StatelessWidget {
     return Container(
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Theme.of(context).colorScheme.onBackground.withOpacity(0.5), width: _getController.height.value * 0.003),
-            color: Theme.of(context).colorScheme.background
+            border: Border.all(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5), width: _getController.height.value * 0.003),
+            color: Theme.of(context).colorScheme.surface
         ),
         margin: EdgeInsets.only(bottom: _getController.height.value * 0.01),
         padding: EdgeInsets.all(_getController.height.value * 0.01),
@@ -30,10 +31,13 @@ class PortItems extends StatelessWidget {
                 child: Column(
                     children: [
                       if (data[0] != '')
-                        SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                        SvgPicture.asset('assets/svg_assets/port.svg',
+                            width: _getController.width.value * 0.022,
+                            height: _getController.height.value * 0.022,
+                            colorFilter: ColorFilter.mode(ports?[0] == 0 ? Colors.orange : ports?[0] == 1 ? Colors.green : ports?[0] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                       else
                         SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                      Text(data[0].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                      Text(data[0].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                     ]
                 )
               )
@@ -45,10 +49,13 @@ class PortItems extends StatelessWidget {
                 child: Column(
                     children: [
                       if (data[1] != '')
-                        SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                        SvgPicture.asset('assets/svg_assets/port.svg',
+                            width: _getController.width.value * 0.022,
+                            height: _getController.height.value * 0.022,
+                            colorFilter: ColorFilter.mode(ports?[1] == 0 ? Colors.orange : ports?[1] == 1 ? Colors.green : ports?[1] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                       else
                         SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                      Text(data[1].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                      Text(data[1].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                     ]
                 )
               )
@@ -60,10 +67,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[2] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[2] == 0 ? Colors.orange : ports?[2] == 1 ? Colors.green : ports?[2] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[2].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[2].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
@@ -75,10 +85,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[3] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[3] == 0 ? Colors.orange : ports?[3] == 1 ? Colors.green : ports?[3] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[3].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[3].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
@@ -90,10 +103,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[4] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[4] == 0 ? Colors.orange : ports?[4] == 1 ? Colors.green : ports?[4] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[4].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[4].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
@@ -105,10 +121,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[5] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[5] == 0 ? Colors.orange : ports?[5] == 1 ? Colors.green : ports?[5] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[5].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[5].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
@@ -120,7 +139,10 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[6] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[6] == 0 ? Colors.orange : ports?[6] == 1 ? Colors.green : ports?[6] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
                           Text(data[6].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
@@ -134,10 +156,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[7] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[7] == 0 ? Colors.orange : ports?[7] == 1 ? Colors.green : ports?[7] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[7].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[7].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
@@ -148,10 +173,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[8] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[8] == 0 ? Colors.orange : ports?[8] == 1 ? Colors.green : ports?[8] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[8].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[8].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
@@ -162,10 +190,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[9] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[9] == 0 ? Colors.orange : ports?[9] == 1 ? Colors.green : ports?[9] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[9].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[9].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
@@ -177,10 +208,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[10] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[10] == 0 ? Colors.orange : ports?[10] == 1 ? Colors.green : ports?[10] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[10].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[10].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
@@ -192,10 +226,13 @@ class PortItems extends StatelessWidget {
                     child: Column(
                         children: [
                           if (data[11] != '')
-                            SvgPicture.asset('assets/svg_assets/port.svg', width: _getController.width.value * 0.022, height: _getController.height.value * 0.022,colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn))
+                            SvgPicture.asset('assets/svg_assets/port.svg',
+                                width: _getController.width.value * 0.022,
+                                height: _getController.height.value * 0.022,
+                                colorFilter: ColorFilter.mode(ports?[11] == 0 ? Colors.orange : ports?[11] == 1 ? Colors.green : ports?[11] == 2 ? Colors.yellow : Theme.of(context).colorScheme.onSurface, BlendMode.srcIn))
                           else
                             SizedBox(width: _getController.width.value * 0.03, height: _getController.height.value * 0.022),
-                          Text(data[11].tr, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: _getController.width.value * 0.03))
+                          Text(data[11].tr, style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: _getController.width.value * 0.03))
                         ]
                     )
                 )
