34f7f5f05d1ff6ef225d8db182b79417c1e4123f
to-rex <torex.amaki@gmail.com>
2024-07-01 05:20:26 +0500

    first commit

--- lib/pages/sample/switch_detail.dart
+++ lib/pages/sample/switch_detail.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:get/get.dart';
 import 'package:hicom/controllers/api_controller.dart';
