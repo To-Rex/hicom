9abeed77cdae38bec1663fcc92530ac80a425f15
to-rex <torex.amaki@gmail.com>
2024-07-19 15:54:43 +0500

    first commit

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,7 +1,5 @@
-import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:get/get.dart';
 import 'package:hicom/companents/instrument/instrument_components.dart';
 import 'package:hicom/controllers/api_controller.dart';
