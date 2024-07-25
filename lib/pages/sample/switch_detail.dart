7f896be90c3102548d413aa7d1e072509a85710d
to-rex <torex.amaki@gmail.com>
2024-07-25 07:55:12 +0500

    first commit

--- lib/pages/sample/switch_detail.dart
+++ lib/pages/sample/switch_detail.dart
@@ -33,7 +33,7 @@ class SwitchDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //_getController.whileApi.value = true;
+    _getController.whileApi.value = true;
     ApiController().getSwitchDetailRealTime(pidId.toString(), sn.toString(),true);
     _getController.tabController = TabController(length: 3, vsync: Navigator.of(context) as TickerProvider);
     _getController.tabController.animateTo(1);
